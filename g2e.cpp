929ad635b72b9dddbd11a03e1fc1046ddf17818a
henok_t_k <you@example.com>
2024-12-21 18:34:00 +0300

    tested accurate for days lapsed since Eth newyear

--- g2e.cpp
+++ g2e.cpp
@@ -7,16 +7,11 @@ vector<int> greg_month_count = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int isLeapYear(int);
 int calc_days_passed(int, int);
 unordered_map<int, int> century_code = {
-    {1800, 3}, {1900, 2}, {2000, 1}, {2100, 1}};
-
-int generateId(int, int);
-int specialId = 91752;
-int id;
+    {1800, 2}, {1900, 1}, {2000, 0}, {2100, 0}};
 
 int main() {
   int month, year;
   cin >> month >> year;
-  ::id = generateId(month, year);
   cout << calc_days_passed(month, year);
 
   return 0;
@@ -29,42 +24,38 @@ int isLeapYear(int year) {
   return 0;
 }
 
-int isJulianLeapYear(int year) {
-  if (year % 4 == 0)
-    return 1;
-  return 0;
-}
 int calc_days_passed(int month, int year) {
-  int day = century_code[ceil(float(year) / 100) * 100];
-  cout << day << endl;
+  const int day = 1;
   int days_passed = 0;
-  greg_month_count[1] += isLeapYear(year);
+  vector<int> month_count = greg_month_count;
+  month_count[1] += isLeapYear(year);
+
   int ethiopianNewYearDay;
-  if (month < 9) {
+  if (month <= 9) {
     ethiopianNewYearDay = (year - 8) % 4 == 0 ? 12 : 11;
   } else {
     ethiopianNewYearDay = (year - 7) % 4 == 0 ? 12 : 11;
   }
-  if (month >= 9) {
-    days_passed += day - ethiopianNewYearDay;
-    for (int i = 8; i < month - 1; i++) {
-      days_passed += greg_month_count[i];
+
+  cout << " " << ethiopianNewYearDay << endl;
+  if (month > 9) {
+    days_passed += day - ethiopianNewYearDay + month_count[8];
+    for (int i = 9; i < month - 1; i++) {
+      days_passed += month_count[i];
     }
     days_passed += day;
   } else {
     for (int i = 8; i < 12; i++) {
-      days_passed += greg_month_count[i];
+      days_passed += month_count[i];
     }
     days_passed += greg_month_count[8] - ethiopianNewYearDay + day;
     for (int i = 0; i < month - 1; i++) {
-      days_passed += greg_month_count[i];
+      days_passed += month_count[i];
     }
-    days_passed += day;
+    days_passed += day - 30;
   }
-  return days_passed;
-}
-
-int generateId(int month, int year) {
-  int digits = year == 0 ? 1 : int(log10(year) + 1);
-  return month * pow(10, digits) + year;
+  int century_id = ceil(float(year) / 100) * 100;
+  int offset = century_code[century_id];
+  offset += (month < 9 && year <= 1901 && year - 1 == century_id);
+  return days_passed + offset;
 }
