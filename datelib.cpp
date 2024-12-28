f01931b2ff4c87ad8c5c0ed6961046ede7235b86
henok_t_k <you@example.com>
2024-12-28 17:07:44 +0300

    fixed bunch of mistake, smooth transition exists

--- datelib.cpp
+++ datelib.cpp
@@ -1,16 +1,14 @@
-#include <vector>
-#include <unordered_map>
-#include <iostream>
 #include "datelib.h"
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-
 unordered_map<int, int> century_code = {
-    {1800, 1}, {1900, 1}, {2000, 0}, {2100, 0}};
+    {1800, 2}, {1900, 1}, {2000, 0}, {2100, 0}};
 
 vector<int> greg_month_count = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
 
 unordered_map<int, int> greg_century_code = {{1700, 4}, {1800, 2}, {1900, 0},
                                              {2000, 6}, {2100, 4}, {2200, 2},
@@ -23,7 +21,7 @@ int calc_days_passed_greg(int month, int year) {
   int days_passed = 0;
   vector<int> month_count = greg_month_count;
   month_count[1] += isGregLeapYear(year);
-
+  int century_id;
   int ethiopianNewYearDay;
   if (month <= 9) {
     ethiopianNewYearDay = (year - 8) % 4 == 0 ? 12 : 11;
@@ -31,14 +29,15 @@ int calc_days_passed_greg(int month, int year) {
     ethiopianNewYearDay = (year - 7) % 4 == 0 ? 12 : 11;
   }
 
-  // cout << " " << ethiopianNewYearDay << endl;
   if (month > 9) {
+    century_id = ceil(float(year + 1) / 100) * 100;
     days_passed += day - ethiopianNewYearDay + month_count[8];
     for (int i = 9; i < month - 1; i++) {
       days_passed += month_count[i];
     }
     days_passed += day;
   } else {
+    century_id = ceil(float(year) / 100) * 100;
     for (int i = 8; i < 12; i++) {
       days_passed += month_count[i];
     }
@@ -48,9 +47,8 @@ int calc_days_passed_greg(int month, int year) {
     }
     days_passed += day - 30;
   }
-  int century_id = ceil(float(year) / 100) * 100;
+
   int offset = century_code[century_id];
-  offset += (month < 9 && year <= 1901 && year - 1 == century_id);
   return days_passed + offset;
 }
 
@@ -62,12 +60,11 @@ int calc_days_passed_jul(int month, int year) {
 
   int ethiopianNewYearDay;
   if (month <= 8) {
-    ethiopianNewYearDay = (year - 8) % 4 == 0 ? 29 : 28;
+    ethiopianNewYearDay = (year - 8) % 4 == 0 ? 30 : 29;
   } else {
-    ethiopianNewYearDay = (year - 7) % 4 == 0 ? 29 : 28;
+    ethiopianNewYearDay = (year - 7) % 4 == 0 ? 30 : 29;
   }
 
-  // cout << " " << ethiopianNewYearDay << endl;
   if (month > 8) {
     days_passed += day - ethiopianNewYearDay + month_count[7];
     for (int i = 8; i < month - 1; i++) {
@@ -82,17 +79,18 @@ int calc_days_passed_jul(int month, int year) {
     for (int i = 0; i < month - 1; i++) {
       days_passed += month_count[i];
     }
-    days_passed += day - 30;
+    // days_passed += day - 30;
+    days_passed -= 30;
   }
   return days_passed;
 }
 
-int calc_days_passed(int month, int year){
-    if (year < 1752 || (year == 1752 && month <= 9)){
-        // cout << "here";
-        return calc_days_passed_jul(month, year);
-    }
-    return calc_days_passed_greg(month, year);
+int calc_days_passed(int month, int year) {
+  if (year < 1752 || (year == 1752 && month <= 9)) {
+
+    return calc_days_passed_jul(month, year);
+  }
+  return calc_days_passed_greg(month, year);
 }
 
 int get_leap_year_code(int month, int year) {
@@ -105,8 +103,7 @@ int get_leap_year_code(int month, int year) {
 
 int isGregLeapYear(int year) {
   // checks if the date is Gregorean Leap Year
-  if (((year % 4 == 0 && year % 400 != 100) || year % 400 == 0) &&
-      (year > 1752))
+  if (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) && (year > 1752))
     return 1;
   else
     return 0;
@@ -119,14 +116,19 @@ int isJulianLeapYear(int year) {
   return 0;
 }
 
-int ethMaxDays(int month,int year){
-    if (year < 1752 || (year == 1752 && month <= 8)){
-        if (month <=8 ) return 365 + int((year - 8) % 4 == 0);
-        else return 365 + int((year - 7) % 4 == 0);
+int ethMaxDays(int month, int year) {
+  // returns maximum ethiopian date 365 || 366
+  year += 1;
+  if (year < 1752 || (year == 1752 && month <= 8)) {
+    if (month <= 8)
+      return 365 + int((year - 8) % 4 == 0);
+    else
+      return 365 + int((year - 7) % 4 == 0);
+  } else {
+    if (month <= 9)
+      return 365 + int((year - 8) % 4 == 0);
+    else {
+      return 365 + int((year - 7) % 4 == 0);
     }
-    else{
-        if (month <= 9) return 365 + int((year - 8) % 4 == 0);
-        else {return 365 + int((year - 7) % 4 == 0); }
-    }
-
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,41 +1,42 @@
+#include "datelib.h"
 #include <cmath>
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-#include "datelib.h"
-#include <cmath>
-
 
 using namespace std;
 
+struct Cell {
 
-
-struct Cell{
-
-  string gregVal= "     |";
+  string gregVal = "     |";
   string ethVal = "     |";
 
-  void set_greg_val(int num){ 
-      int i = 4;
-      while (num > 0){
-        gregVal[i] = char(num%10 + '0');
-      i-=1;
+  void set_greg_val(int num) {
+    int i = 4;
+    while (num > 0) {
+      gregVal[i] = char(num % 10 + '0');
+      i -= 1;
       num /= 10;
-      } 
+    }
   }
-  void set_eth_val(int num){
-    if (num == 0) num = 30;
+  void set_eth_val(int num, int max) {
+    if (num > max)
+      num -= max;
+    if (num > 360)
+      num -= 360;
+    num %= 30;
+    if (num == 0)
+      num = 30;
     int i = int(log10(num));
-      while (num > 0){
-        ethVal[i] = char(num%10 + '0');
-        i-=1;
-        num /= 10;
-      }
+    while (num > 0) {
+      ethVal[i] = char(num % 10 + '0');
+      i -= 1;
+      num /= 10;
+    }
   }
-
 };
 
-void print(vector<vector<int>>&, int, int);
+void print(vector<vector<int>> &, int, int);
 vector<vector<int>> con_matirx(int, int);
 
 int main() {
@@ -44,56 +45,60 @@ int main() {
   vector<vector<int>> mat = con_matirx(month, year);
   int ethDay = calc_days_passed(month, year);
   int ethMax = ethMaxDays(month, year);
+  cout << "max: " << ethMax << endl;
   print(mat, ethDay, ethMax);
 
   return 0;
 }
 
-void print(vector<vector<int>> &mat, int ethDay, int ethMax){
+void print(vector<vector<int>> &mat, int ethDay, int ethMax) {
 
   cout << "|-----------------------------------------|" << endl;
   cout << "|Sun  |Mon  |Tue  |Wed  |Thu  |Fri  |Sat  |" << endl;
   cout << "|-----------------------------------------|" << endl;
 
-  for (int r = 0; r < mat.size(); r++){
+  for (int r = 0; r < mat.size(); r++) {
     cout << "|";
     Cell cell[7];
-    for (int c = 0; c < mat[r].size() ; c++){
+    for (int c = 0; c < mat[r].size(); c++) {
       cell[c].set_greg_val(mat[r][c]);
-      int temp = ethDay-int(ethDay/ethMax);
-      if (mat[r][c]!=0) {cell[c].set_eth_val((temp)%30); ethDay++;}
-      
+      // int temp = ethDay - int(ethDay / ethMax);
+      if (mat[r][c] != 0) {
+        cell[c].set_eth_val(ethDay, ethMax);
+        ethDay++;
+      }
+
       cout << cell[c].gregVal;
       // ethDay ++;
     }
     cout << endl;
 
-
     cout << "|";
-    for (int c = 0; c < mat[r].size(); c++){
+    for (int c = 0; c < mat[r].size(); c++) {
       cout << cell[c].ethVal;
     }
     cout << endl;
     cout << "|-----------------------------------------|" << endl;
   }
-
 }
 
 vector<vector<int>> con_matirx(int month, int year) {
   int century_code;
-  if (year<1752 ||  (month<=9 && year == 1752)){
+  if (year < 1752 || (month <= 9 && year == 1752)) {
     century_code = (18 - year / 100) % 7;
-  }else{
+  } else {
     century_code = greg_century_code[year / 100 * 100];
   }
   // int century_code =
-      // ((18 - year / 100) % 7) * (month <= 9 && year <= 1752) +
-      // (greg_century_code[year / 100 * 100]) * !(month <= 9 && year <= 1752);
+  // ((18 - year / 100) % 7) * (month <= 9 && year <= 1752) +
+  // (greg_century_code[year / 100 * 100]) * !(month <= 9 && year <= 1752);
   int year_code = ((year % 100) + (year % 100) / 4) % 7;
   int leap_year_code = get_leap_year_code(month, year);
   int start_day = (year_code + greg_month_code[month - 1] + century_code + 1 -
                    leap_year_code + 1 * (year < 4)) %
                   7;
+  cout << "leap_year_code" << leap_year_code << endl;
+  cout << "Start day: " << start_day << endl;
   int rows;
   int cols = 7;
   int days = greg_month_count[month - 1] +
@@ -105,7 +110,7 @@ vector<vector<int>> con_matirx(int month, int year) {
   int offset = start_day;
 
   vector<int> single_row(cols, 0);
-  if (!(month==9 && year == 1752)) {
+  if (!(month == 9 && year == 1752)) {
     vector<vector<int>> mat(rows, single_row);
     for (int i = 0; i < days; i++) {
       mat[(offset + i) / cols][(offset + i) % cols] = i + 1;
