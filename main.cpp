e3b841632b2c243e29c1c0efc8130a678546113d
henok_t_k <you@example.com>
2025-01-12 22:32:21 +0300

    better prompt

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main() {
   int month, year;
-  cin >> month >> year;
+  std::cout << "Enter a month 1 - 12: ";
+  cin >> month;
+  cout << "Enter a year in gregorian 1 - 4599: ";
+  cin >> year;
+
   print(month, year);
+  cin.clear();
+  cin.ignore();
+  cout << "Press any key to exit" << endl;
+  cin.get();
+
   return 0;
 }
