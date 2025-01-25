31c0d5f56b8c68a0708c9e8487ccbd667ed4c998
Enoch-Tadesse <henitadesse97@gmail.com>
2025-01-25 22:00:44 +0300

    finalizing the main file with input handling

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include "datelib.h"
+#include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  int month, year;
-  std::cout << "Enter a month 1 - 12: ";
-  cin >> month;
-  cout << "Enter a year in gregorian 1 - 4599: ";
-  cin >> year;
-
-  print(month, year);
-  cin.clear();
-  cin.ignore();
-  cout << "Press any key to exit" << endl;
+label:
+  int year;
+  std::cout << "Enter the calendar year between 1 and 5999: " << std::endl;
+  std::cin >> year;
+  if (year < 1 or year > 5999) {
+    std::cerr << "You entered invalid year. Please try again." << std::endl;
+    std::cin.clear();
+    std::cin.ignore();
+    goto label;
+  }
+  for (int month = 1; month <= 12; month++)
+    print(month, year);
+  std::cout << "Press any key to exit. " << std::endl;
+  std::cin.clear();
+  std::cin.ignore();
   cin.get();
 
   return 0;
