eb5a093ccd0d4ad2a5c817597ddb20b860683bb6
Enoch-Tadesse <henitadesse97@gmail.com>
2025-01-25 21:06:20 +0300

    fixed leap year date errors

--- holib.cpp
+++ holib.cpp
@@ -6,14 +6,14 @@
 typedef std::unordered_map<std::string, std::vector<int>> holiday;
 
 holiday eth_holi = {
-    {"Meskel", {1, 382}},
-    {"Enkutatash", {1, 366}},
-    {"Derg Downfall Day", {8, 260}},
-    {"Veteran's Day", {7, 237}},
-    {"Labour's Day", {7, 233}},
-    {"Adwa Victory Day", {5, 173}},
-    {"Timket", {5, 131}},
-    {"Gena", {4, 119}},
+    {"Meskel", {1, 17, 0}},
+    {"Enkutatash", {1, 1, 0}},
+    {"Derg Downfall Day", {9, 290, 0}},
+    {"Veteran's Day", {7, 237, 0}},
+    {"Labour's Day", {7, 233, 0}},
+    {"Adwa Victory Day", {5, 173, 0}},
+    {"Timket", {5, 131, 0}},
+    {"Gena", {4, 119, 0}},
 };
 
 holiday specialHoliDates(int year) {
@@ -25,23 +25,22 @@ holiday specialHoliDates(int year) {
 std::vector<std::string> get_eth_holidays(int month, int year,
                                           int days_passed) {
   std::vector<std::string> collections;
-  int offset = int((year - 3) % 4 == 0);
   int mod = ethMaxDays(month, year);
   holiday corr_eth_holi = specialHoliDates(year);
 
-  int upper_limit = (days_passed + greg_month_count[month - 1]);
-  int lower_limit = upper_limit - greg_month_count[month - 1];
+  int upper_limit = (days_passed + greg_month_count[month - 1]) % mod;
+  int lower_limit = (upper_limit - greg_month_count[month - 1]) % mod;
 
   for (auto day : corr_eth_holi) {
     if ((day.second[1] % mod < upper_limit &&
          day.second[1] % mod >= lower_limit) ||
         (day.second[1] < upper_limit && day.second[1] >= lower_limit)) {
-      int bin_day = day.second[1] % 365;
+      int bin_day = day.second[1] % mod;
       bin_day = bin_day - ((bin_day - 1) / 30) * 30;
       std::string send =
           day.first + " : " + greg_months[month - 1] + " " +
-          std::to_string((day.second[1] - lower_limit + 1) % mod + offset) +
-          " / " + eth_months[day.second[0] - 1] + " " + std::to_string(bin_day);
+          std::to_string((day.second[1] - lower_limit + 1) % mod) + " / " +
+          eth_months[day.second[0] - 1] + " " + std::to_string(bin_day);
       collections.push_back(send);
     }
   }
