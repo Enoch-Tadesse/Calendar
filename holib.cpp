9e98994c4ed9152453450a3f2f8efb00cf7ae038
Enoch-Tadesse <henitadesse97@gmail.com>
2025-02-14 17:12:12 +0300

    holidays now maintain order

--- holib.cpp
+++ holib.cpp
@@ -2,24 +2,23 @@
 #include <cmath>
 #include <map>
 #include <string>
-#include <unordered_map>
 
-typedef std::unordered_map<std::string, std::vector<int>> holiday;
+typedef std::map<std::vector<int>, std::string> holiday;
 
 holiday eth_holi = {
-    {"Meskel", {1, 17}},
-    {"Enkutatash", {1, 1}},
-    {"Derg Downfall Day", {9, 290}},
-    {"Veteran's Day", {8, 237}},
-    {"Labour's Day", {8, 233}},
-    {"Adwa Victory Day", {6, 173}},
-    {"Timket", {5, 131}},
-    {"Gena", {4, 119}},
+
+    {{1, 1}, "Enkutatash"},
+    {{1, 17}, "Meskel"},
+    {{4, 119}, "Gena"},
+    {{5, 131}, "Timket"},
+    {{6, 173}, "Adwa Victory Day"},
+    {{8, 233}, "Labour's Day"},
+    {{8, 237}, "Veteran's Day"},
+    {{9, 290}, "Derg Downfall Day"},
 }; // the Ethiopian Holidays month with corresponding day since the new year
 
 holiday specialHoliDates(int year) {
   holiday corrected = eth_holi;
-  corrected["Gena"][1] -= int((year - 7) % 4 == 0);
   std::map<int, int> tewsak_relation = {{0, 6}, {1, 5}, {2, 4}, {3, 3},
                                         {4, 2}, {5, 8}, {6, 7}};
   year -= 8; // to convert the gregorean year into Ethiopian year
@@ -40,14 +39,13 @@ holiday specialHoliDates(int year) {
       (metqe + tewsak_relation[tewsak] + 1) % 30; // the nenewe fasting day
   day = (month + 5 - 1) * 30 + nenewe;            // day of nenewe
 
-  corrected.insert({"Nenewe Tsom", {(day - 1) / 30 + 1, day}});
-  corrected.insert({"Tinsae", {(day + 69 - 1) / 30 + 1, day + 69}});
-  corrected.insert({"Seqlet", {(day + 67 - 1) / 30 + 1, day + 67}});
-  corrected.insert({"Abiy Tsom", {(day + 14 - 1) / 30 + 1, day + 14}});
-  corrected.insert({"Yehawaryat Tsom", {(day + 119 - 1) / 30 + 1, day + 119}});
-  corrected.insert({"Debre Zeyt", {(day + 41 - 1) / 30 + 1, day + 41}});
-  corrected.insert({"Hosana", {(day + 62 - 1) / 30 + 1, day + 62}});
-
+  corrected.insert({{(day - 1) / 30 + 1, day}, "Nenewe Tsom"});
+  corrected.insert({{(day + 69 - 1) / 30 + 1, day + 69}, "Tinsae"});
+  corrected.insert({{(day + 67 - 1) / 30 + 1, day + 67}, "Seqlet"});
+  corrected.insert({{(day + 14 - 1) / 30 + 1, day + 14}, "Abiy Tsom"});
+  corrected.insert({{(day + 119 - 1) / 30 + 1, day + 119}, "Yehawaryat Tsom"});
+  corrected.insert({{(day + 41 - 1) / 30 + 1, day + 41}, "Debre Zeyt"});
+  corrected.insert({{(day + 62 - 1) / 30 + 1, day + 62}, "Hosana"});
   return corrected;
 }
 
@@ -63,19 +61,21 @@ std::vector<std::string> get_eth_holidays(int month, int year,
   int lower_limit = (upper_limit - greg_month_count[month - 1]) %
                     mod; // lower limi tof the month to be printed
 
-  for (auto day : corr_eth_holi) {
-    if ((day.second[1] % mod < upper_limit &&
-         day.second[1] % mod >= lower_limit) ||
-        (day.second[1] < upper_limit && day.second[1] >= lower_limit)) {
+  for (auto &day : corr_eth_holi) {
+    bool offset = (day.second == "Gena" && (year - 7) % 4 == 0) ? true : false;
+    // considers leap year for Gena or Christmas
+    int corr_day = day.first[1] - offset; // corrects the day
+    if ((corr_day % mod < upper_limit && corr_day % mod >= lower_limit) ||
+        (corr_day < upper_limit && corr_day >= lower_limit)) {
       /*cheks if any of the holidays fall into the current month being printed
        */
-      int bin_day = day.second[1] % mod;
+      int bin_day = corr_day % mod;
       bin_day =
           bin_day - ((bin_day - 1) / 30) * 30; // gets the correct ethiopian day
-      std::string send =
-          day.first + " : " + greg_months[month - 1] + " " +
-          std::to_string((day.second[1] - lower_limit + 1) % mod) + " / " +
-          eth_months[day.second[0] - 1] + " " + std::to_string(bin_day);
+      std::string send = day.second + " : " + greg_months[month - 1] + " " +
+                         std::to_string((corr_day - lower_limit + 1) % mod) +
+                         " / " + eth_months[day.first[0] - 1] + " " +
+                         std::to_string(bin_day);
       collections.push_back(send);
     }
   }
--- holib.h
+++ holib.h
@@ -1,9 +1,9 @@
 #pragma once
 
+#include <map>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
-extern std::unordered_map<int, std::string> eth_holi;
+extern std::map<int, std::string> eth_holi;
 
 std::vector<std::string> get_eth_holidays(int, int, int);
